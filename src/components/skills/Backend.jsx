fc1729b7943e1532d7cabbfffc834bc11ff92e09
posternaka <nik.petrenko97@gmail.com>
2024-04-23 22:21:37 +0300

    developed and styled section-skills

---
+++ src/components/skills/Backend.jsx
@@ -0,0 +1,60 @@
+const Backend = () => {
+	return (
+		<div className='skills__content'>
+			<h3 className='skills__title'>Backend developer</h3>
+
+			<div className='skills__box'>
+				<div className='skills__group'>
+					<div className='skills__data'>
+						<i className='uil uil-check-circle'></i>
+
+						<div>
+							<h3 className='skills__name'>Node.js</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i className='uil uil-check-circle'></i>
+
+						<div>
+							<h3 className='skills__name'>Express.js</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+				</div>
+
+				<div className='skills__group'>
+					<div className='skills__data'>
+						<i className='uil uil-check-circle'></i>
+
+						<div>
+							<h3 className='skills__name'>Node.js</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i className='uil uil-check-circle'></i>
+
+						<div>
+							<h3 className='skills__name'>Express.js</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+
+					<div className='skills__data'>
+						<i className='uil uil-check-circle'></i>
+
+						<div>
+							<h3 className='skills__name'>MySQL</h3>
+							<span className='skills__level'>Advanced</span>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Backend;
