2393cd9b221558444aee3afc298974c1928899a9
posternaka <nik.petrenko97@gmail.com>
2024-04-29 21:21:17 +0300

    configured sending messages from the form to my email

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,6 +1,33 @@
+import emailjs from '@emailjs/browser';
+import { useRef, useState } from 'react';
 import './contact.css';
 
 const Contact = () => {
+	const form = useRef();
+	const [statusMessage, setStatusMessage] = useState({
+		success: false,
+		error: false,
+	});
+
+	const sendEmail = e => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm('service_3gggg0l', 'template_z5ft2yo', form.current, {
+				publicKey: 'Ny65md4izLxaMjQaA',
+			})
+			.then(
+				() => {
+					setStatusMessage({ success: true, error: false });
+					e.target.reset();
+				},
+				(error) => {
+					setStatusMessage({ success: false, error: true });
+					console.log(error);
+				}
+			);
+	};
+
 	return (
 		<section className='contact section' id='contact'>
 			<h2 className='section__title'>Get in touch</h2>
@@ -60,7 +87,7 @@ const Contact = () => {
 				<div className='contact__content'>
 					<h3 className='contact__title'>Write me your project</h3>
 
-					<form className='contact__form'>
+					<form className='contact__form' ref={form} onSubmit={sendEmail}>
 						<div className='contact__form-div'>
 							<label className='contact__form-tag'>Name</label>
 							<input
@@ -76,7 +103,7 @@ const Contact = () => {
 							<label className='contact__form-tag'>Mail</label>
 							<input
 								type='email'
-								name='name'
+								name='email'
 								className='contact__form-input'
 								placeholder='Insert your email'
 								required
@@ -92,6 +119,36 @@ const Contact = () => {
 								className='contact__form-input'
 								placeholder='Write your project'
 							></textarea>
+						</div>
+
+						<div
+							className={
+								statusMessage.success
+									? 'contact__form-message message-success contact__form-message-active'
+									: 'contact__form-message message-success'
+							}
+						>
+							<div className='contact__form-success-icon'>
+								<i className='uil uil-check-circle'></i>
+							</div>
+							<div className='contact__form-success-text'>
+								<p>Your message has been sent</p>
+							</div>
+						</div>
+
+						<div
+							className={
+								statusMessage.error
+									? 'contact__form-message message-error contact__form-message-active'
+									: 'contact__form-message message-error'
+							}
+						>
+							<div className='contact__form-error-icon'>
+								<i className='uil uil-times-circle'></i>
+							</div>
+							<div className='contact__form-error-text'>
+								<p>Something went wrong</p>
+							</div>
 						</div>
 
 						<button className='button button--flex'>
